7fd92e3c94feed1d975825593191ee7aa6f72e46
Kanchan Tyagi <kanchan.tyagi8@gmail.com>
2020-01-12 20:49:46 +0530

    Updates

---
+++ precache-manifest.4a160653131a9568ae7dd27b963d2ab8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a453c5aba2f36aff98b7c66a8de1969",
+    "url": "/kanchanportfolio/index.html"
+  },
+  {
+    "revision": "38bb611234addee16468",
+    "url": "/kanchanportfolio/static/css/main.a1b80f76.chunk.css"
+  },
+  {
+    "revision": "f317628cca84bde1a278",
+    "url": "/kanchanportfolio/static/js/2.96207c57.chunk.js"
+  },
+  {
+    "revision": "38bb611234addee16468",
+    "url": "/kanchanportfolio/static/js/main.df5aa787.chunk.js"
+  },
+  {
+    "revision": "eedaafc94de9ea46f730",
+    "url": "/kanchanportfolio/static/js/runtime-main.7f66538b.js"
+  },
+  {
+    "revision": "f81834e0fe40e24da5dc31e179e56096",
+    "url": "/kanchanportfolio/static/media/access-management-system.f81834e0.png"
+  },
+  {
+    "revision": "24c598888a4a3a621d8f79ce260589f8",
+    "url": "/kanchanportfolio/static/media/adeo-abudhabi.24c59888.png"
+  },
+  {
+    "revision": "dbcd9e02ce41508d4cf1610d4f6e27ab",
+    "url": "/kanchanportfolio/static/media/cventimg.dbcd9e02.png"
+  },
+  {
+    "revision": "ef0026a3715987aca62eae8521b65abb",
+    "url": "/kanchanportfolio/static/media/idfcfirst-banking.ef0026a3.png"
+  },
+  {
+    "revision": "2f4e6dd9250764ee91f79324e38e60cf",
+    "url": "/kanchanportfolio/static/media/mynewpic.2f4e6dd9.jpg"
+  },
+  {
+    "revision": "b060a3dfa0b960f1e7e04b89964e8e6e",
+    "url": "/kanchanportfolio/static/media/netspend.b060a3df.png"
+  },
+  {
+    "revision": "ba016dc6405349f5467cee7dbd8855c9",
+    "url": "/kanchanportfolio/static/media/newgen-img.ba016dc6.png"
+  }
+]);
